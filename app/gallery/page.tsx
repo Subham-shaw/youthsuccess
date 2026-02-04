7394b4c19e2d2ec6ddd735ffa76c83d27ab432bc
Subham-shaw <subhamshaw.prime@gmail.com>
2026-02-04 19:42:57 +0530

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -5,25 +5,26 @@ import Footer from '../components/Footer';
 import fs from "fs";
 import path from "path";
 
+const galleryDir = path.join(process.cwd(), "public/gallery");
+
+const images = fs
+  .readdirSync(galleryDir)
+  .filter((f) => /\.(png|jpe?g|webp)$/i.test(f))
+  .map((f) => `/gallery/${f}`);
+
 
 
 
 const Gallery = () => {
-  const galleryDir = path.join(process.cwd(), "public/gallery");
-
-  const images = fs
-    .readdirSync(galleryDir)
-    .filter((file) => /\.(png|jpe?g|webp|gif)$/i.test(file))
-    .map((file) => `/gallery/${file}`);
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <section className="relative flex w-screen flex-col items-center gap-[10vh] py-[10vh]">
-      {images.map((img, idx) => (
-        <StickyCard_003 key={idx} imgUrl={img} />
-      ))}
-    </section>
-    <Footer/>
+        {images.map((img, idx) => (
+          <StickyCard_003 key={idx} imgUrl={img} />
+        ))}
+      </section>
+      <Footer />
     </>
   )
 }
