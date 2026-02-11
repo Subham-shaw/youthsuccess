d9f1e52909fad95d2f7ae50f358f86726a75524d
Subham-shaw <subhamshaw.prime@gmail.com>
2026-02-11 18:38:14 +0530

    Update ImagesBadgeButton.tsx

--- app/components/assets/ImagesBadgeButton.tsx
+++ app/components/assets/ImagesBadgeButton.tsx
@@ -6,7 +6,7 @@ import { ImagesBadge } from "@/components/ui/images-badge";
 export function ImagesBadgeButton() {
   return (
     <div>
-      <Link href="https://drive.google.com/drive/folders/1dvAbupXkztRDqfCwBugwzSDJMOL_GE6j?usp=drive_link">
+      <Link href="https://drive.google.com/drive/folders/1dvAbupXkztRDqfCwBugwzSDJMOL_GE6j?usp=drive_link" target="_blank">
         <ImagesBadge
           text="Explore Plans"
           images={[
